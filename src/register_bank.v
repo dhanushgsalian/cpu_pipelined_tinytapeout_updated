0d3f6a62c59863f43b60f6d561cbb9dfeeea4995
dhanushgsalian <159764200+dhanushgsalian@users.noreply.github.com>
2025-08-07 12:58:55 +0530

    Update register_bank.v

--- src/register_bank.v
+++ src/register_bank.v
@@ -1,4 +1,4 @@
-module register_bank #(parameter DEPTH = 15, WIDTH = 8, ADD_WIDTH = 5) (clk, w_en, r_reg1, r_reg2, w_reg, w_data, read_data1, read_data2); //This implements the 16 32-bit registers and read and write to register only seven reigiters are used remaining dedicated to future use
+module register_bank #(parameter DEPTH = 15, WIDTH = 8, ADD_WIDTH = 5) (clk, w_en, r_reg1, r_reg2, w_reg, w_data, read_data1, read_data2, output_data); //This implements the 16 32-bit registers and read and write to register only seven reigiters are used remaining dedicated to future use
   input clk;
   input w_en;
   input [ADD_WIDTH - 1:0] r_reg1;
@@ -7,6 +7,7 @@ module register_bank #(parameter DEPTH = 15, WIDTH = 8, ADD_WIDTH = 5) (clk, w_e
   input [WIDTH-1:0] w_data;
   output [WIDTH-1:0] read_data1;
   output [WIDTH-1:0] read_data2;
+  output [WIDTH-1:0] output_data;
 
 	reg [WIDTH - 1 : 0] registers[0 : DEPTH - 1]; //8 bit  (8x15) register creation
 
@@ -18,5 +19,6 @@ module register_bank #(parameter DEPTH = 15, WIDTH = 8, ADD_WIDTH = 5) (clk, w_e
 
   assign read_data1 = registers[r_reg1];        //based on addres data reading from registers asynchronously
   assign read_data2 = registers[r_reg2];        //based on addres data reading from registers asynchronously
+  assign output_data = registers[w_reg];
 
 endmodule
