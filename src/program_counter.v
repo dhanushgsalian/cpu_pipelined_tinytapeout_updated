0e985aa2b494181298b12510bbbc2088e7b9f015
dhanushgsalian <159764200+dhanushgsalian@users.noreply.github.com>
2025-08-21 14:41:21 +0530

    Update program_counter.v

--- src/program_counter.v
+++ src/program_counter.v
@@ -1,6 +1,7 @@
-module program_counter #(parameter WIDTH = 32)(clk, rst, pc_scr, jump_add,current_ins_add);//this module compute next instruction address
+module program_counter #(parameter WIDTH = 32)(clk, rst, condition, pc_scr, jump_add,current_ins_add);//this module compute next instruction address
   input clk;
   input rst;
+  input condition;
   input [6:0] pc_scr;
   input [6:0] jump_add;
   output reg [WIDTH-1:0] current_ins_add = {WIDTH{1'b0}};
@@ -12,11 +13,17 @@ module program_counter #(parameter WIDTH = 32)(clk, rst, pc_scr, jump_add,curren
 	    current_ins_add <= {WIDTH{1'b0}};
     end
     else if(pc_scr == 7'b1111111)begin       //to implement halt
-      current_ins_add <= current_ins_add;      //next instruction address is assigned to current instruction address
+        current_ins_add <= current_ins_add;      //next instruction address is assigned to current instruction address
     end
-    else if(pc_scr == 7'b000_0000) begin
+    else if(pc_scr == 7'b110_1111) begin
         current_ins_add <= jump_add;
-     end 
+     end
+     else if(pc_scr == 7'b110_0011) begin
+        if(condition)
+            current_ins_add <= jump_add;
+        else
+            current_ins_add <= next_ins_add;       
+     end
 	 else begin
 		current_ins_add <= next_ins_add;
 	 end
